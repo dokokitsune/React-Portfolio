4ac72976d53997158b20997703686151b55e30e4
dokokitsune <stakin101@gmail.com>
2023-09-06 14:24:18 -0700

    first commit

---
+++ src/componants/contact/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Contact_Me extends React.Component {
+
+  render() {
+    return(
+      <>
+
+      </>
+    )
+  }
+}
+
+export default Contact_Me;
---
+++ src/componants/navbar/Navi.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar"
+
+class Navi extends React.Component {
+
+  render() {
+    return(
+      <>
+        <Navbar expand="lg" className="bg-body-tertiary"></Navbar>
+          <Container>
+            
+          </Container>
+      </>
+    
+
+    )
+  }
+
+}
+
+export default Navi;
