936f1253618022dcbdf3edd04c1fbcbedf095e52
dokokitsune <stakin101@gmail.com>
2023-09-17 19:38:01 -0700

    transfer

--- src/componants/navbar/Navi.js
+++ src/componants/navbar/Navi.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { Container, NavDropdown, Navbar, NavbarBrand } from "react-bootstrap";
+import { Container, Navbar } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import "bootstrap/dist/css/bootstrap.css"
 
 import "./Navi.scss"
+import { NavLink, useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 
 function Navi(){
 
 
+  const [navActive, setNavActive] = React.useState(null);
 
+  const location = useLocation();
 
+  React.useEffect(() => {
+    setNavActive(location.pathname);
+
+  }  ,[location]);
 
     return(
       <>
@@ -19,17 +27,17 @@ function Navi(){
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav className="ms-auto" variant="underline">
-              <Nav.Link href="/" className="nav-item">Home</Nav.Link>
-                {/* <NavDropdown title="About Me" className="nav-item" id="about-dropdown">
+            
+              <Nav.Link href="/" active={navActive === "/"}  >Home</Nav.Link>
+                {/* <NavDropdown title="About Me" className="nav-item" id="about-dropdown">style={({isActive}) => (isActive ? (color: '#029cd7') : (color: 'white'))}
                   <NavDropdown.Item href="/about" className="nav-dropdown-text">Summery</NavDropdown.Item>
-                  <NavDropdown.Item href="/projects" className="nav-dropdown-text">My Projects</NavDropdown.Item>
+                  <NavDropdown.Item href="/projects" className="nav-dropdown-text">My Projects</NavDropdown.Item>#029cd7;
                 </NavDropdown> */}
-                <Nav.Link href="/about" className="nav-item">About Me</Nav.Link>
-                <Nav.Link href="/skills" className="nav-item">Skills</Nav.Link>
-                <Nav.Link href="/education" className="nav-item">Education</Nav.Link>
-                <Nav.Link href="/projects" className="nav-item">Projects</Nav.Link>
-                <Nav.Link href="/contact" className="nav-item">Contact Me</Nav.Link>
-                <Nav.Link href="/resume" className="nav-item">Resume</Nav.Link>
+                <Nav.Link href="/about" active={navActive === "/about"} className="nav-item">About Me</Nav.Link>
+                <Nav.Link href="/skills" active={navActive === "/skills"} className="nav-item">Skills</Nav.Link>
+                <Nav.Link href="/projects" active={navActive === "/projects"} className="nav-item">Projects</Nav.Link>
+                <Nav.Link href="/contact" active={navActive === "/contact"} className="nav-item">Contact Me</Nav.Link>
+                <Nav.Link href="/resume" active={navActive === "/resume"} className="nav-item">Resume</Nav.Link>
               </Nav>
 
           </Navbar.Collapse>
