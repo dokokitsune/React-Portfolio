724d8dae578001e061dc90734a8f86c8e1304174
dokokitsune <stakin101@gmail.com>
2023-09-27 14:27:29 -0700

    added aws-amplify for backend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
 import { BrowserRouter } from 'react-router-dom';
+
+Amplify.configure(config);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
