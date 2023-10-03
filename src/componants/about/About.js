5a8c8c915fbfda60f84dd97388ac71a7ba4731cf
dokokitsune <stakin101@gmail.com>
2023-10-02 22:19:47 -0700

    Added a discription for About-Me

--- src/componants/about/About.js
+++ src/componants/about/About.js
@@ -31,8 +31,12 @@ function About_Me(){
     return (
       <>
 
-        <div className="aboutMe translate-middle-x start-50 top-50 position-absolute">
-          Hello testing testing
+        <div className="aboutMe start-50 top-50 position-absolute ">
+          My name is Weston Wood. I am currently a Junior at California State University, Los Angeles
+          and a Web Officer for the Assosiation for Computing Machinery (ACM) on Campus. After college
+          I want to persue a Backend Web-development job or a Dev-ops engineer job. In my free time I work
+          on learning new skills for my future career.
+
 
         </div>
 
